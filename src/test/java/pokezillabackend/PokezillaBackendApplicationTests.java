224aad8d049108c8de7eced8e2ef28c4904be9d9
Maria Manoela <manoelamvc@hotmail.com>
2022-10-06 08:11:13 -0300

    build: initializing project

---
+++ src/test/java/pokezillabackend/PokezillaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pokezillabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokezillaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
